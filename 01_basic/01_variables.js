bff59b1b929ea220fb826cb8504903a3d89b2fcf
prashantmishra25 <144652305+prashantmishra25@users.noreply.github.com>
2024-08-31 18:30:20 +0000

    data

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,25 @@
+const account_Id = 144532
+let accountEmail = "pmmis@oogle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//you can either use semicolon or not it depend on you
+
+/*
+generaly we are not use var because
+pf issue in block scope and functional scope
+*/
+
+// account_Id = 2 not allowed
+
+accountEmail = "ps@oogle.com"
+accountPassword = "145"
+accountCity = "Jai"
+
+// console.log(account_Id);
+// console.log(accountEmail)
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([ account_Id ,accountEmail, accountPassword , accountCity, accountState ])
+
+
---
+++ 01_basic/02_data.js
@@ -0,0 +1,33 @@
+/*
+in previous java function their is absence of classes module arrowfunction but in present it is found
+*/
+"use strict";//treat all js code as newer version but not to be write 
+
+//alert("hello")
+//alert( 3 + 3) we are using node.js not browser
+
+console.log(3+3); 
+console.log("hitesh") // code readibility must be high it means we have to write in way that it is easy to understand
+
+let name = "prashant"
+let age = 18
+let islogged = false
+let state = null // difference between null and zero in case of temprature it is clearly understand
+
+/*number => 2 to power 52
+bigint
+string => "" 
+boolean => true/false
+null => it is standalone value
+undefined =>    
+symbol => used for uniqness
+
+
+object*/
+
+// console.log(typeof("asdf")); // used  for to find the type
+// console.log(typeof null)
+console.table([typeof(1234), typeof("asdf") , typeof(null) , typeof(undefined)])
+
+
+
